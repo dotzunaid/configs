9d891d50ebd5b14da76e42a18ccd5f147fdcd8e9
SZunaidA <fakehotdog03@gmail.com>
2025-11-26 02:30:21 +1100

    install and setup lazydev

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -112,11 +112,30 @@ require("lazy").setup({
           completion = { documentation = { auto_show = true } },
 
           sources = {
-            default = { 'lsp', 'path', 'snippets', 'buffer' },
+            default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer' },
+               providers = {
+                  lazydev = {
+                    name = "LazyDev",
+                    module = "lazydev.integrations.blink",
+                    -- make lazydev completions top priority (see `:h blink.cmp`)
+                    score_offset = 100,
+                  },
+               },
           },
 
           fuzzy = { implementation = "prefer_rust_with_warning" }
         },
         opts_extend = { "sources.default" }
-    }
+    },
+    {
+        "folke/lazydev.nvim",
+        ft = "lua", -- only load on lua files
+        opts = {
+          library = {
+            -- See the configuration section for more details
+            -- Load luvit types when the `vim.uv` word is found
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+          },
+        },
+    },
 })

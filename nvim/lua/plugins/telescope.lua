ef50b25860f215fdedfbeec0d4f85b9592c0e8ef
SZunaidA <fakehotdog03@gmail.com>
2025-12-15 00:38:51 +1100

    make telescope be able to find hidden files

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -7,5 +7,8 @@ return {
        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
        vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
        vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+       vim.keymap.set("n", "<leader>fa", function()
+           builtin.find_files({ no_ignore = true, hidden = true })
+       end, { desc = "Find files including gitignored and hidden" })
    end,
 }
